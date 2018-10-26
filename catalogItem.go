268142e055ceef63f0eef5fd0fdbd430599bb5ee
Attila Mitrocsak <attila.mitrocsak@gmail.com>
2018-10-26 07:12:50 +0100

    Added catalogItem type

---
+++ catalogItem.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"time"
+
+	"github.com/pkg/errors"
+	"github.com/spf13/afero"
+)
+
+type catalogItem struct {
+	Path             string    `json:"path"`
+	Size             int64     `json:"size"`
+	ModificationTime time.Time `json:"modification_time"`
+	Md5Sum           string    `json:"md5sum"`
+}
+
+// newCatalogItem creates a catalogItem for the specified file
+func newCatalogItem(fs afero.Fs, path string) (*catalogItem, error) {
+	buf, err := afero.ReadFile(fs, path)
+	if err != nil {
+		return nil, errors.Wrap(err, "Cannot open file")
+	}
+
+	fi, err := fs.Stat(path)
+	if err != nil {
+		return nil, errors.Wrap(err, "Cannot get file info")
+	}
+
+	hash := md5.New()
+	hash.Write(buf)
+	return &catalogItem{
+		Path:             path,
+		Size:             fi.Size(),
+		ModificationTime: fi.ModTime(),
+		Md5Sum:           hex.EncodeToString(hash.Sum(nil)),
+	}, nil
+}
---
+++ catalogItem_test.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"testing"
+
+	th "github.com/mitro42/testhelper"
+	"github.com/spf13/afero"
+)
+
+func TestMissingFile(t *testing.T) {
+	item, err := newCatalogItem(afero.NewOsFs(), "no_such_file")
+	th.NokPrefix(t, err, "Cannot open file")
+	th.Assert(t, item == nil, "Item expected to be nil")
+}
+
+func TestCatalogItem(t *testing.T) {
+	path := "test_data/test1.txt"
+	item, err := newCatalogItem(afero.NewOsFs(), path)
+	th.Ok(t, err)
+	th.Equals(t, path, item.Path)
+	th.Equals(t, int64(1160), item.Size)
+	th.Equals(t, "2018-10-24 23:38:47.713775685 +0100 BST", item.ModificationTime.String())
+	th.Equals(t, "b3cd1cf6179bca32fd5d76473b129117", item.Md5Sum)
+}
+
+func TestCatalogItem2(t *testing.T) {
+	path := "test_data/test2.txt"
+	item, err := newCatalogItem(afero.NewOsFs(), path)
+	th.Ok(t, err)
+	th.Equals(t, path, item.Path)
+	th.Equals(t, int64(1304), item.Size)
+	th.Equals(t, "2018-10-25 07:37:27.809296805 +0100 BST", item.ModificationTime.String())
+	th.Equals(t, "89b2b34c7b8d232041f0fcc1d213d7bc", item.Md5Sum)
+}
