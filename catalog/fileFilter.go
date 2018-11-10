783e174f6deff263ceca098b34f20be271fb6dd2
Attila Mitrocsak <attila.mitrocsak@gmail.com>
2018-11-10 20:15:04 +0000

    Added file filters

---
+++ catalog/fileFilter.go
@@ -0,0 +1,35 @@
+package catalog
+
+import "strings"
+
+// FileFilter is an interface for filters used in the catalog scanning pipeline
+type FileFilter interface {
+	Include(path string) bool
+}
+
+// ExtensionFilter filters the files based on their extension. The listed extensions
+// will be excluded
+func ExtensionFilter(extensions ...string) FileFilter {
+	return extensionFilter{
+		extensions: extensions,
+	}
+}
+
+type noFilter struct{}
+
+func (e noFilter) Include(path string) bool {
+	return true
+}
+
+type extensionFilter struct {
+	extensions []string
+}
+
+func (e extensionFilter) Include(path string) bool {
+	for _, ext := range e.extensions {
+		if strings.HasSuffix(path, "."+ext) {
+			return false
+		}
+	}
+	return true
+}
---
+++ catalog/fileFilter_test.go
@@ -0,0 +1,44 @@
+package catalog
+
+import (
+	"testing"
+
+	th "github.com/mitro42/testhelper"
+)
+
+func TestNoFilter(t *testing.T) {
+	f := noFilter{}
+	th.Equals(t, true, f.Include(""))
+	th.Equals(t, true, f.Include("a"))
+	th.Equals(t, true, f.Include("a.jpg"))
+	th.Equals(t, true, f.Include("a.txt"))
+	th.Equals(t, true, f.Include("folder/another/file.txt"))
+	th.Equals(t, true, f.Include("folder/another/"))
+}
+
+func TestExtensionFilter(t *testing.T) {
+	f := ExtensionFilter("txt")
+	th.Equals(t, true, f.Include(""))
+	th.Equals(t, true, f.Include("a"))
+	th.Equals(t, true, f.Include("a.jpg"))
+	th.Equals(t, false, f.Include("a.txt"))
+	th.Equals(t, false, f.Include("folder/another/file.txt"))
+	th.Equals(t, true, f.Include("folder/another/"))
+}
+
+func TestExtensionFilterMulti(t *testing.T) {
+	f := ExtensionFilter("txt", "jpg")
+	th.Equals(t, true, f.Include(""))
+	th.Equals(t, true, f.Include("a"))
+	th.Equals(t, false, f.Include("a.jpg"))
+	th.Equals(t, false, f.Include("a.txt"))
+	th.Equals(t, false, f.Include("folder/another/file.txt"))
+	th.Equals(t, true, f.Include("include/this/txt/file.png"))
+	th.Equals(t, true, f.Include("include/this/too/filetxt"))
+	th.Equals(t, true, f.Include("include/this/too/filejpg"))
+	th.Equals(t, true, f.Include("include/this/too/fileJP"))
+	th.Equals(t, true, f.Include("include/this/too/file.JPG"))
+	th.Equals(t, true, f.Include("include/this/too/file.TXT"))
+	th.Equals(t, true, f.Include("include/this/too/file.TxT"))
+	th.Equals(t, true, f.Include("folder/another/"))
+}
